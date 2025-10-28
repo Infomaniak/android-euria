66299509380e623d04c834bdfa8b494b5a888a9f
Vincent Te <vincent.te@infomaniak.com>
2025-10-28 18:50:05 +0100

    chore: Remove Singleton

--- app/src/main/java/com/infomaniak/euria/di/TwoFactorAuthManagerModule.kt
+++ app/src/main/java/com/infomaniak/euria/di/TwoFactorAuthManagerModule.kt
@@ -27,13 +27,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.flow.flowOf
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object TwoFactorAuthManagerModule {
 
-    @Singleton
     @Provides
     fun providesTwoFactorAuthManager(
         userSharedPref: UserSharedPref,
