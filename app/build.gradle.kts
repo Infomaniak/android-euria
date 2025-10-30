79666294fd9806bc6fb1dde51be62b103c538257
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-10-30 17:50:16 +0100

    chore: New release 0.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.infomaniak.euria"
         minSdk = appMinSdk
         targetSdk = appTargetSdk
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         buildConfigField("String", "CLIENT_ID", "\"10476B29-7B98-4D42-B06B-2B7AB0F06FDE\"")
         buildConfigField("String", "EURIA_URL", "\"https://euria.infomaniak.com/\"")
