c007a05b73cbde6bb45ea0b8be11fa73866b4e0a
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2026-03-18 15:48:05 +0100

    chore: Start using composite build

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,8 @@ buildscript {
 }
 
 plugins {
-    alias(libs.plugins.android.application) apply false
+    alias(core.plugins.android.application) apply false
+    alias(core.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.serialization) apply false
     alias(core.plugins.kapt) apply false
