73e30f1a853058e5ad015d24873a43d4b330d37e
Vincent Te <vincent.te@infomaniak.com>
2026-06-02 13:00:20 +0200

    chore(Version): 1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.infomaniak.euria"
         minSdk = appMinSdk
         targetSdk = appTargetSdk
-        versionCode = 1_004_001_01
-        versionName = "1.4.1"
+        versionCode = 1_004_002_01
+        versionName = "1.4.2"
 
         buildConfigField("String", "CLIENT_ID", "\"10476B29-7B98-4D42-B06B-2B7AB0F06FDE\"")
 
