24ce9c732c85a6fcf7bf74a5d697a0f409c272e1
Vincent Te <vincent.te@infomaniak.com>
2026-07-08 09:47:45 +0200

    chore: Change windowSoftInputMode to adjustNothing to see if it changes anything on foldable for the scrolling issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         applicationId = "com.infomaniak.euria"
         minSdk = appMinSdk
         targetSdk = appTargetSdk
-        versionCode = 1_004_003_01
+        versionCode = 1_004_003_02
         versionName = "1.4.3"
 
         buildConfigField("String", "CLIENT_ID", "\"10476B29-7B98-4D42-B06B-2B7AB0F06FDE\"")
