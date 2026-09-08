0b8e2066d4f1adac19c5cd075cfec928696b12cd
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-09-08 08:46:05 +0200

    fix: Deprecated extra getter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,10 +31,10 @@ plugins {
     alias(core.plugins.sentry.plugin)
 }
 
-val appCompileSdk: Int by rootProject.extra
-val appTargetSdk: Int by rootProject.extra
-val appMinSdk: Int by rootProject.extra
-val javaVersion: JavaVersion by rootProject.extra
+val appCompileSdk = rootProject.extra["appCompileSdk"] as Int
+val appTargetSdk = rootProject.extra["appTargetSdk"] as Int
+val appMinSdk = rootProject.extra["appMinSdk"] as Int
+val javaVersion = rootProject.extra["javaVersion"] as JavaVersion
 
 android {
 
